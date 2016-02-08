0efbe3d0a80b1170bdcf538981b0f9bef2c17904
Evelyn Utterson <e.utterson@hotmail.co.uk>
2016-02-08 20:16:29 +0000

    install body-parser

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ var app = express();
 var port = '3000';
 
 var films = require('./models/films.js');
+var disneyRouter = require('./controllers/disneyRouter');
+
+var expressLayouts = require('express-ejs-layouts');
+
+// Application Settings
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+app.use('/films', disneyRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+
 
 app.get('/', function(req, res){
   res.send('Disney films');
---
+++ controllers/disneyRouter.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var disneyRouter = express.Router();
+
+module.exports = disneyRouter;
