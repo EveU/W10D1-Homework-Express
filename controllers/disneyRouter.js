497d694a960f6a087f0f24b468b81b3c9894fb5a
Evelyn Utterson <e.utterson@hotmail.co.uk>
2016-02-08 21:36:00 +0000

    add new form

--- controllers/disneyRouter.js
+++ controllers/disneyRouter.js
@@ -10,10 +10,24 @@ disneyRouter.get('/', function(req, res){
   res.render('films/index', {films: films});
 });
 
+// NEW
+disneyRouter.get('/new', function(req, res){
+  res.render('films/new');
+});
+
+//CREATE
+disneyRouter.post('/', function(req, res){
+  var newFilm = {}
+  newFilm.title = req.body.title;
+  newFilm.year = parseInt(req.body.year);
+  newFilm.runTime = parseInt(req.body.runTime);
+  films.unshift(newFilm);
+  res.redirect('/');
+});
+
 // SHOW
 disneyRouter.get('/:id', function(req, res){
   res.render('films/show', {film: films[req.params.id], filmId: req.params.id});
 });
-
 
 module.exports = disneyRouter;
