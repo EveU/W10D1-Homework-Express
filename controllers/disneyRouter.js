de071f43bd7358ba98b241a2aa9e31093d521e49
Evelyn Utterson <e.utterson@hotmail.co.uk>
2016-02-08 21:49:47 +0000

    add edit/update

--- controllers/disneyRouter.js
+++ controllers/disneyRouter.js
@@ -15,7 +15,7 @@ disneyRouter.get('/new', function(req, res){
   res.render('films/new');
 });
 
-//CREATE
+// CREATE
 disneyRouter.post('/', function(req, res){
   var newFilm = {}
   newFilm.title = req.body.title;
@@ -29,5 +29,21 @@ disneyRouter.post('/', function(req, res){
 disneyRouter.get('/:id', function(req, res){
   res.render('films/show', {film: films[req.params.id], filmId: req.params.id});
 });
+
+// EDIT
+disneyRouter.get('/:id/edit', function(req, res){
+  res.render('films/edit', {film: films[req.params.id], filmId: req.params.id});
+});
+
+// UPDATE
+disneyRouter.post('/:id', function(req, res){
+  var film = films[req.params.id];
+  film.title = req.body.title || film.title;
+  film.year = parseInt(req.body.year) || film.year;
+  film.runTime = parseInt(req.body.runTime) || film.runTime;
+  films[req.params.id] = film;
+  res.redirect('/');
+});
+
 
 module.exports = disneyRouter;
